e4f06c828a7addb67e9498a1bafed54b5fb88c34
Rannatpf <rannatpf@gmail.com>
2026-04-15 15:05:37 -0300

    feat-dashboard-levelSalao

--- Desktop/PROJETOS/DashSalaoLevel/dashboard/main.py
+++ Desktop/PROJETOS/DashSalaoLevel/dashboard/main.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import requests
-import plotly.express as px
 import plotly.graph_objects as go
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import LabelEncoder
